67580aab1658309f484656e76b68c44af9b72f5e
Ajmal Ansari <ajmal@Ajmals-MacBook-Pro.local>
2025-02-26 10:02:54 +0000

    add opacity on click

--- week5/exercise1/script.js
+++ week5/exercise1/script.js
@@ -1,8 +1,8 @@
 
 function onClickCircle(){
     var button = document.getElementById("circle");
-    
-    button.style.opacity = 1;
+    var circleOpacity = parseFloat(button.style.opacity);
+    button.style.opacity = circleOpacity + 0.1;
 }
 function onLoadFunction(){
     var button = document.getElementById("circle");
