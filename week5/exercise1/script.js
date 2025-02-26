6d77da464fea270f25389159534b06ccccb89d19
Ajmal Ansari <ajmal@Ajmals-MacBook-Pro.local>
2025-02-26 10:00:24 +0000

    ex4

--- week5/exercise1/script.js
+++ week5/exercise1/script.js
@@ -1,11 +1,12 @@
 
 function onClickCircle(){
     var button = document.getElementById("circle");
-    button.style.opacity = 0.5;
-    button.style.opacity = 0.5;
+    
+    button.style.opacity = 1;
 }
 function onLoadFunction(){
     var button = document.getElementById("circle");
+    button.style.opacity = 0.5;
     button.addEventListener('click', onClickCircle)
 }
 document.addEventListener('DOMContentLoaded', onLoadFunction);
