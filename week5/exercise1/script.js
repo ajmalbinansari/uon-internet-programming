0d0eecb149f617a79992e1ae86108f969e9c3477
Ajmal Ansari <ajmal@Ajmals-MacBook-Pro.local>
2025-02-26 10:03:31 +0000

    reduce opacity on click

--- week5/exercise1/script.js
+++ week5/exercise1/script.js
@@ -2,11 +2,11 @@
 function onClickCircle(){
     var button = document.getElementById("circle");
     var circleOpacity = parseFloat(button.style.opacity);
-    button.style.opacity = circleOpacity + 0.1;
+    button.style.opacity = circleOpacity - 0.1;
 }
 function onLoadFunction(){
     var button = document.getElementById("circle");
-    button.style.opacity = 0.5;
+    button.style.opacity = 1;
     button.addEventListener('click', onClickCircle)
 }
 document.addEventListener('DOMContentLoaded', onLoadFunction);
